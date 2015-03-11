47673f811b6db0ead89839803677d52b11bb45f3
Emanuele Cisbani <ecisbani@intesigroup.com>
2015-03-11 17:48:31 +0100

    widget error code info

--- otp.php
+++ otp.php
@@ -35,17 +35,25 @@ if (isset($_GET['uID'])) {
 if (isset($_GET['msg'])) {
 	//$response = $_GET['msg'];
 	$response = $_GET;
-	/* You should manage some bad responses, most relevant cases are:
-		1) user did not completed successfully the enrollement 
-			(i.e. her entered wrong OTP)
-		2) max user reached (you can prevent this usually but
-			concurrent enrollements may occur...)
-	*/
+/* You should manage responses, most relevant cases are:
+1001 = Well done! The service is now enabled
+1000 = The service is not available or authorized (TransactionID is empty)
+0401 = An unexpected error occurred on the server, retry later
+0402 = Service data are not available, retry later
+0403 = The company user has already activated the service with a different Time4Mind credentials
+0404 = The Time4Mind user has already activated the service with a different company username
+0405 = The service has reached the maximum number of users allowed
+       (you can prevent this usually but concurrent enrollements may occur...)
+0406 = SESSION EXPIRED OR NOT VALID
+ 257 = The service is not available or authorized (TransactionID is empty)
+ 515 = Time at your disposal is expired! (TransactionID expired)
+ 516 = TRANSACTION ID IS NOT VALID
+*/
 	}
 ?>
 
 <!DOCTYPE html><html><head> <title>Time4eID DEMO</title> </head>
-<p>username: <?php echo $_SESSION['username'];?></p>
+<p>username: <?php print_r ($_SESSION['username']);?></p>
 <p>Please, insert the One Time Password generated on your smartphone with the app Valid
 <form role="form" name="formProfile" method="post">
        	<input type="text" name="otp" autofocus autocomplete="off" required placeholder="OTP" >
