b87dea281dcf42015e48591e9850e5ea8e66d28b
Emanuele Cisbani <ecisbani@intesigroup.com>
2015-03-13 13:22:35 +0100

    config cleaned

--- config.php
+++ config.php
@@ -4,21 +4,11 @@
 $cert_name = '/var/www/ssl/certificate.pem';
 $cert_pwd = 'password';
 
-/*
-// hostname for WIDGET
-$_SESSION['WIDGET'] = 'https://valid.time4mind.com/serv/getCompanyServiceInfo.php';
-
-// URLs for RPC
-$time4user_methods = 'https://sme.time4mind.com/Time4UserServices/services/backend/t4ujson';
-$time4id_methods = 'https://sme.time4mind.com/Time4eIDv2/backend/auth';
-*/
-
 /* Collaudo */
 $_SESSION['WIDGET'] = 'https://valid.test4mind.com/serv/getCompanyServiceInfo.php';
 $time4user_methods = 'https://sme.test4mind.com/Time4UserServices/services/backend/t4ujson';
 $time4id_methods = 'https://sme.test4mind.com/Time4eID/backend/auth';
 
-// questo deve restare in collaudo anche se faccio merge
 
 ?>
 
