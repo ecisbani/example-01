fc0fb38d68336d2dcf49d66ca1805db8e59f9f16
Emanuele Cisbani <ecisbani@intesigroup.com>
2015-03-13 13:23:30 +0100

    config cleaned

--- config.php
+++ config.php
@@ -4,22 +4,12 @@
 $cert_name = '/var/www/ssl/certificate.pem';
 $cert_pwd = 'password';
 
-/*
 // hostname for WIDGET
 $_SESSION['WIDGET'] = 'https://valid.time4mind.com/serv/getCompanyServiceInfo.php';
 
 // URLs for RPC
 $time4user_methods = 'https://sme.time4mind.com/Time4UserServices/services/backend/t4ujson';
 $time4id_methods = 'https://sme.time4mind.com/Time4eIDv2/backend/auth';
-*/
-
-// questo deve restare nel branch master anche se faccio merge
-
-/* Collaudo */
-$_SESSION['WIDGET'] = 'https://valid.test4mind.com/serv/getCompanyServiceInfo.php';
-$time4user_methods = 'https://sme.test4mind.com/Time4UserServices/services/backend/t4ujson';
-$time4id_methods = 'https://sme.test4mind.com/Time4eID/backend/auth';
-
 
 ?>
 
