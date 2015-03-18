948ed355d7fd0c0e452ad2439b7104400ba77f2c
Emanuele Cisbani <emanuele.cisbani@gmail.com>
2015-03-18 11:41:23 +0100

    added error codes list for getTransactionID

--- enroll.php
+++ enroll.php
@@ -4,7 +4,13 @@ include 'functions.php';
 
 // associate a TransactionID to the username
 $_SESSION['TransactionID'] = getTransacionID($_SESSION['username']);    
-// TODO: list error code to manage
+/* You should manage some bad responses, most relevant cases are:
+257: param externaUserId can not be empty
+768: error when try to load data from database
+17154: user in not attested to any node
+17155: node authorization failed
+17664: service non found on nodeId where user is attested
+*/
 
 // redirect the browser to the Valid pages to complete the enroll procedure
 if ($_SESSION['TransactionID']!='') {
