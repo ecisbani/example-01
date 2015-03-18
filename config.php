0ac3933b2fe7dc71f1891ddce48c68a98e54b862
Emanuele Cisbani <emanuele.cisbani@gmail.com>
2015-03-18 19:29:22 +0100

    config collaudo

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 
 // Credentials PEM file and password
-$cert_name = '/var/www/ssl/certificate.pem';
+$cert_name = '/var/www/ssl/certificate.pem_collaudo';
 $cert_pwd = 'password';
 
 // hostname for WIDGET
-$_SESSION['WIDGET'] = 'https://valid.time4mind.com/serv/getCompanyServiceInfo.php';
+$_SESSION['WIDGET'] = 'https://valid.test4mind.com/serv/getCompanyServiceInfo.php';
 
 // URLs for RPC
-$time4user_methods = 'https://sme.time4mind.com/Time4UserServices/services/backend/t4ujson';
-$time4id_methods = 'https://sme.time4mind.com/Time4eIDv2/backend/auth';
+$time4user_methods = 'https://sme.test4mind.com/Time4UserServices/services/backend/t4ujson';
+$time4id_methods = 'https://sme.test4mind.com/Time4eID/backend/auth';
 
 ?>
 
