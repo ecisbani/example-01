8abd5228d5900c0b89c1c77687a934eb7804873a
Emanuele Cisbani <emanuele.cisbani@gmail.com>
2015-05-14 12:06:28 +0200

    config standard

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 
 // Credentials PEM file and password
-$cert_name = '/var/www/ssl/certificate.pem_integration';
+$cert_name = '/var/www/ssl/certificate.pem';
 $cert_pwd = 'password';
 
 // hostname for WIDGET
-$_SESSION['WIDGET'] = 'https://valid-int.time4mind.com/serv/getCompanyServiceInfo.php';
+$_SESSION['WIDGET'] = 'https://valid.time4mind.com/serv/getCompanyServiceInfo.php';
 
 // URLs for RPC
-$time4user_methods = 'https://sme-int.time4mind.com/Time4UserServices/services/backend/t4ujson';
-$time4id_methods = 'https://sme-int.time4mind.com/Time4eID/backend/auth';
+$time4user_methods = 'https://sme.time4mind.com/Time4UserServices/services/backend/t4ujson';
+$time4id_methods = 'https://sme.time4mind.com/Time4eID/backend/auth';
 
 ?>
 
